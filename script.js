954c81a45ec6a15300d8707e0abcbbb1968bcae1
titanxrio <Titan150208@gmail.com>
2025-03-14 16:38:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Öffnet das Modal und zeigt das angeklickte Produktbild an
+// Öffnet das Modal und zeigt das angeklickte Produktbild im Overlay an
 function openModal(element) {
   const modal = document.getElementById('modal');
   const modalImg = document.getElementById('modal-img');
-  // Hole das Bild aus der angeklickten Produktkarte
   const img = element.querySelector('img');
   modalImg.src = img.src;
   modal.style.display = 'flex';
 }
 
-// Schließt das Modal, wenn außerhalb des Inhalts oder auf den Close-Button geklickt wird
+// Schließt das Modal, wenn außerhalb des Inhalts oder auf den Schließ-Button geklickt wird
 function closeModal(event) {
   const modalContent = document.querySelector('.modal-content');
   if (event.target.classList.contains('close-btn') || !modalContent.contains(event.target)) {
